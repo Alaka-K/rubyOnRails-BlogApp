a4986ee201d057d34929d165521a3fd0afc5441b
kehinde alaka <kennyalaka93@gmail.com>
2022-01-16 18:43:30 +0000

    Added new route that is accessible in the show action of the controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   root "articles#index"
   get "/articles", to: "articles#index" # this route declares that GET /articles requests 
   #are mapped to the index action of ArticlesController.
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
