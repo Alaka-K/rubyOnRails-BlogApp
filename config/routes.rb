153359ca6d4b569edd3c088e85142d645f6462d0
kehinde alaka <kennyalaka93@gmail.com>
2022-01-16 19:24:48 +0000

    refactored the two get request to resources. used article_path helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index" # this route declares that GET /articles requests 
-  #are mapped to the index action of ArticlesController.
-  get "/articles/:id", to: "articles#show"
+
+  resources :articles
 end
